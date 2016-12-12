2a653fdc7d219cc206f574d35e60076714e73af6
zdnexus <zdnexus@163.com>
2016-12-12 21:09:05 +0800

    ~

--- demo-ES6/1.变量申明/2.变量提升.js
+++ demo-ES6/1.变量申明/2.变量提升.js
@@ -1,24 +1,24 @@
 'use strict';
 
-console.log(x1); // undefined (var变量提升)
-// console.log(y1); // 报错 (let不支持变量提升)
-var x1 = 1;
-let y1 = 2;
+console.log(a); // undefined (var变量提升)
+// console.log(b); // ReferenceError: a is not defined (let不支持变量提升)
+var a = 1;
+let b = 2;
+console.log(a); // 1
+console.log(b); // 2
 
-
-var x2 = 3;
-function func2() {
-    console.log(x2);//3 访问全局变量
+var a = 3;
+function f1() {
+    console.log(a);//3 访问全局变量
 }
-func2();
+f1();
 
-
-var x3 = 3;
-function func3() {
-    console.log(x3);// undefined
-    var x3 = 'a';// 变量提升
+var a = 4;
+function f2() {
+    console.log(a);// undefined
+    var a = 5;// 变量提升
 }
-func3();
+f2();
 
 
 var x4 = 3;
