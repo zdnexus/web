dc3ac0a900c50a90bbb4f4f6277685fa55218357
zdnexus <zdnexus@163.com>
2017-09-24 15:36:56 +0800

    ~~~

--- test.js
+++ test.js
@@ -2,5 +2,4 @@
  * Created by Administrator on 2017/4/7 0007.
  */
 
-console.log(parseFloat('123').toFixed(2));
-console.log(parseFloat(undefined).toFixed(2));
+console.log(a);
--- 坑/跨域/jsonp.js
+++ 坑/跨域/jsonp.js
@@ -12,66 +12,66 @@ function ajax(params) {
     var json = params.jsonp ? jsonp(params) : json(params);
 
     // ajax请求
-    // function json(params) {
-    //     // 请求方式，默认是GET
-    //     params.type = (params.type || 'GET').toUpperCase();
-    //     // 避免有特殊字符，必须格式化传输数据
-    //     params.data = formatParams(params.data);
-    //
-    //     var xhr = null;
-    //     // 实例化XMLHttpRequest对象
-    //     if (window.XMLHttpRequest) {
-    //         xhr = new XMLHttpRequest();
-    //     }
-    //     // IE6及其以下版本
-    //     else {
-    //         xhr = new ActiveXObjcet('Microsoft.XMLHTTP');
-    //     }
-    //
-    //     // 监听事件，只要 readyState 的值变化，就会调用 readystatechange 事件
-    //     xhr.onreadystatechange = function () {
-    //         //  readyState属性表示请求/响应过程的当前活动阶段，4为完成，已经接收到全部响应数据
-    //         if (xhr.readyState == 4) {
-    //             var status = xhr.status;
-    //
-    //             //  status：响应的HTTP状态码，以2开头的都是成功
-    //             if (status >= 200 && status < 300) {
-    //                 var response = '';
-    //                 // 判断接受数据的内容类型
-    //                 var type = xhr.getResponseHeader('Content-type');
-    //
-    //                 if (type.indexOf('xml') !== -1 && xhr.responseXML) {
-    //                     response = xhr.responseXML; //Document对象响应
-    //                 }
-    //                 else if (type === 'application/json') {
-    //                     response = JSON.parse(xhr.responseText); //JSON响应
-    //                 } else {
-    //                     response = xhr.responseText; //字符串响应
-    //                 }
-    //                 // 成功回调函数
-    //                 params.success && params.success(response);
-    //
-    //             } else {
-    //                 params.error && params.error(status);
-    //             }
-    //         }
-    //     };
-    //
-    //
-    //     // 连接和传输数据
-    //     if (params.type == 'GET') {
-    //         // 三个参数：请求方式、请求地址(get方式时，传输数据是加在地址后的)、是否异步请求(同步请求的情况极少)；
-    //         xhr.open(params.type, params.url + '?' + params.data, true);
-    //         xhr.send(null);
-    //     }
-    //     else {
-    //         xhr.open(params.type, params.url, true);
-    //         //必须，设置提交时的内容类型
-    //         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    //         // 传输数据
-    //         xhr.send(params.data);
-    //     }
-    // // }
+    function json(params) {
+        // 请求方式，默认是GET
+        params.type = (params.type || 'GET').toUpperCase();
+        // 避免有特殊字符，必须格式化传输数据
+        params.data = formatParams(params.data);
+
+        var xhr = null;
+        // 实例化XMLHttpRequest对象
+        if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        }
+        // IE6及其以下版本
+        else {
+            xhr = new ActiveXObjcet('Microsoft.XMLHTTP');
+        }
+
+        // 监听事件，只要 readyState 的值变化，就会调用 readystatechange 事件
+        xhr.onreadystatechange = function () {
+            //  readyState属性表示请求/响应过程的当前活动阶段，4为完成，已经接收到全部响应数据
+            if (xhr.readyState == 4) {
+                var status = xhr.status;
+
+                //  status：响应的HTTP状态码，以2开头的都是成功
+                if (status >= 200 && status < 300) {
+                    var response = '';
+                    // 判断接受数据的内容类型
+                    var type = xhr.getResponseHeader('Content-type');
+
+                    if (type.indexOf('xml') !== -1 && xhr.responseXML) {
+                        response = xhr.responseXML; //Document对象响应
+                    }
+                    else if (type === 'application/json') {
+                        response = JSON.parse(xhr.responseText); //JSON响应
+                    } else {
+                        response = xhr.responseText; //字符串响应
+                    }
+                    // 成功回调函数
+                    params.success && params.success(response);
+
+                } else {
+                    params.error && params.error(status);
+                }
+            }
+        };
+
+
+        // 连接和传输数据
+        if (params.type == 'GET') {
+            // 三个参数：请求方式、请求地址(get方式时，传输数据是加在地址后的)、是否异步请求(同步请求的情况极少)；
+            xhr.open(params.type, params.url + '?' + params.data, true);
+            xhr.send(null);
+        }
+        else {
+            xhr.open(params.type, params.url, true);
+            //必须，设置提交时的内容类型
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+            // 传输数据
+            xhr.send(params.data);
+        }
+    }
 
     // jsonp请求
     function jsonp(params) {
