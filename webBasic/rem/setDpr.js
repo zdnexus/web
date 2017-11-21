37687bcdbb7d463a2155e6df94c808359fc29cb2
zdnexus <zdnexus@163.com>
2017-11-21 18:45:19 +0800

    ～～～～

---
+++ webBasic/rem/setDpr.js
@@ -0,0 +1,108 @@
+;(function(win, lib) {
+    let timer;
+    let doc = win.document;
+    let docEl = doc.documentElement;
+    let metaEl = doc.querySelector('meta[name="viewport"]');
+    let flexibleEl = doc.querySelector('meta[name="flexible"]');
+    let dpr = 0;
+    let scale = 0;
+    let flexible = lib.flexible || (lib.flexible = {});
+
+    if(metaEl){
+        let contentMatch=metaEl.getAttribute('content').match(/initial\-scale=([\d\.]+)/);
+        if(contentMatch){
+            scale = parseFloat(contentMatch[1]);
+            dpr = parseInt(1/scale);
+        }
+    }else if(flexibleEl){
+        let content=flexible.getAttribute('content');
+        if(content){
+            let initialDpr=content.match(/initial\-dpr=([\d\.]+)/);
+            let maximumDpr=content.match(/maximum\-dpr=([\d\.]+)/);
+            if(initialDpr){
+                dpr = parseFloat(initialDpr[1]);
+                scale = parseFloat((1 / dpr).toFixed(2));
+            }
+            if(maximumDpr){
+                dpr = parseFloat(maximumDpr[1]);
+                scale = parseFloat((1 / dpr).toFixed(2));
+            }
+        }
+    }
+    if (!dpr && !scale) {
+        let isAndroid = win.navigator.appVersion.match(/android/gi);
+        let isIPhone = win.navigator.appVersion.match(/iphone/gi);
+        let devicePixelRatio = win.devicePixelRatio;
+        if (isIPhone) {
+            // iOS下，对于2和3的屏，用2倍的方案，其余的用1倍方案
+            if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {
+                dpr = 3;
+            } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)){
+                dpr = 2;
+            } else {
+                dpr = 1;
+            }
+        } else {
+            // 其他设备下，仍旧使用1倍的方案
+            dpr = 1;
+        }
+        scale = 1 / dpr;
+    }
+    docEl.setAttribute('data-dpr', dpr);
+
+    if (!metaEl) {
+        metaEl = doc.createElement('meta');
+        metaEl.setAttribute('name', 'viewport');
+        metaEl.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no');
+        if (docEl.firstElementChild) {
+            docEl.firstElementChild.appendChild(metaEl);
+        } else {
+            let wrap = doc.createElement('div');
+            wrap.appendChild(metaEl);
+            doc.write(wrap.innerHTML);
+        }
+    }
+
+    function refreshRem() {
+        let width = docEl.getBoundingClientRect().width;
+        width / dpr > 540 && (width = 540 * dpr);
+        let rem = width / 12;
+        docEl.style.fontSize = rem + "px";
+        flexible.rem = win.rem = rem;
+        docEl.style.width = "100%";
+        docEl.style.height = "100%";
+    }
+
+    win.addEventListener('resize', function() {
+        clearTimeout(timer);
+        timer = setTimeout(refreshRem, 300);
+    }, false);
+
+    win.addEventListener('pageshow', function(e) {
+        if (e.persisted) {
+            clearTimeout(timer);
+            timer = setTimeout(refreshRem, 300);
+        }
+    }, false);
+
+    if (doc.readyState === 'complete') {
+        doc.body.style.fontSize = 12 * dpr + 'px';
+    } else {
+        doc.addEventListener('DOMContentLoaded', function(e) {
+            doc.body.style.fontSize = 12 * dpr + 'px';
+        }, false);
+    }
+    refreshRem();
+
+    flexible.dpr=win.dpr=dpr;
+    flexible.refreshRem=refreshRem;
+    flexible.rem2px=function(px){
+        let rem=parseFloat(px)*this.rem;
+        return "string" == typeof px && px.match(/rem$/) && (rem += "px"), rem ;
+    }
+    flexible.px2rem=function(rem2){
+        let px=parseFloat(rem2)/this.rem;
+        return "string" == typeof rem2 && rem2.match(/px$/) && (px += "rem"), px ;
+    }
+
+})(window, window['lib'] || (window['lib'] = {}));
