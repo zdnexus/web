66ff75f3b12075b7d92146a7b59bc8b22a361963
zdnexus <zdnexus@163.com>
2022-05-30 20:25:42 +0800

    学习cookie-session-token

--- test.js
+++ test.js
@@ -21,82 +21,69 @@
 // // p.sayName();
 
 
-var list = [
-    {
-        a: 1
-    },
-    {
-        b: 1
-    }
-]
-var p = new Person();
+// var list = [
+//   {
+//     a: 1
+//   },
+//   {
+//     b: 1
+//   }
+// ]
+// var p = new Person();
+//
+// // Person.prototype.name = 'nexus';
+//
+// Person.prototype = {
+//   constructor: Person,
+//   name: 'noa',
+//   sayHi: function () {
+//     console.log('hi');
+//   }
+// }
+//
+// var list_2 = [];
+// var one = list[0];
+// list_2.push(one);
+// console.log(list)
+// console.log(list_2)
+//
+// one.a = 3;
+// console.log(p.name);
+// // p.sayName();
+//
+//
+// let a = 1;
+//
+// let b = function (a) {
+//   console.log(arguments);
+//   switch (a) {
+//     case 1: {
+//       return 10;
+//     }
+//     case 2: {
+//       return 20;
+//     }
+//   }
+// };
+// let c = (...a) => {
+//   console.log(a);
+//   switch (a) {
+//     case 1: {
+//       return 10;
+//     }
+//     case 2: {
+//       return 20;
+//     }
+//   }
+// };
+// console.log(b(a));
+// console.log(c(a));
 
-// Person.prototype.name = 'nexus';
-
-Person.prototype = {
-    constructor: Person,
-    name: 'noa',
-    sayHi: function () {
-        console.log('hi');
-    }
-}
-
-var list_2 = [];
-var one = list[0];
-list_2.push(one);
-console.log(list)
-console.log(list_2)
-
-one.a = 3;
-console.log(p.name);
-// p.sayName();
+const L1 = [1, 2]
+console.log(L1.includes(1))
+console.log(L1.includes(2))
+const L2 = [{ a: 1 }, { b: 2 }]
+console.log(L2.includes({ a: 1 }))
+console.log(L2.includes({ b: 2 }))
 
 
-let a = 1;
-
-let b = function (a) {
-    console.log(arguments);
-    switch (a) {
-        case 1:
-        {
-            return 10;
-        }
-        case 2:
-        {
-            return 20;
-        }
-    }
-};
-let c = (...a) => {
-    console.log(a);
-    switch (a) {
-        case 1:
-        {
-            return 10;
-        }
-        case 2:
-        {
-            return 20;
-        }
-    }
-};
-console.log(b(a));
-console.log(c(a));
-
-
-var s = '#/orderDetail/orderNo=20180111018569481';
-s = s.substr(s.lastIndexOf('/') + 1);
-console.log(s);
-
-function parseQueryString(url) {
-    var iterms = url.split("&");
-    var arr, Json = {};
-    for (var i = 0; i < iterms.length; i++) {
-        arr = iterms[i].split("=");
-        Json[arr[0]] = arr[1];
-    }
-    return Json;
-}
-
-var o = parseQueryString(s);
-console.log(o.orderNo);
--- watch/cookie-session-token/index.js
+++ watch/cookie-session-token/index.js
@@ -1,17 +1,67 @@
 const express = require('express')
-const e_session = require('express-session')
+const express_session = require('express-session')
 
 const app = express()
 
-const name = 'larryzhong'
-const password = '123456'
+const DB = [
+  {
+    username: 'larryzhong',
+    password: '123456'
+  }
+]
+let session_db
 
-app.use(e_session({
-  secret: 'larryzhong的签名',     // 给cookie-sessionId签名
-  name: 'larryzhong的sessionId', // 默认sid
+app.use(express_session({
+  secret: 'larryzhong_secret',  // 加签
+  name: 'larryzhong_name',      // 默认sid
   cookie: {
     maxAge: 15000
   },
   resave: false,
   saveUninitlized: false
 }))
+
+app.get('/', (req, res) => {
+  console.log('用户访问了页面')
+  session_db = req.session
+  console.log(session_db)
+  if (session_db.username) {
+    console.log('用户有登录态')
+    res.send(
+      `<h1>欢迎回来，${session_db.username}</h1>
+        <a href="/logout">退出</a>`
+    )
+  } else {
+    console.log('用户没有登录态')
+    res.sendFile(__dirname + '/index.html')
+  }
+})
+
+app.post('/login', (req, res) => {
+  console.log('用户登录')
+  console.log(req.body)
+  console.log(res.body)
+  if (DB.filter(item => item.username === req.body.username && item.password === req.body.password)) {
+    console.log('用户登录成功')
+    session_db = req.session
+    session_db.username = req.body.username
+    res.send(
+      `<h1>欢迎回来，${session_db.username}</h1>
+        <a href="/logout">退出</a>`
+    )
+  } else {
+    console.log('用户登录失败')
+    res.send('用户名或者密码错误')
+  }
+})
+
+app.get('/logout', (req, res) => {
+  console.log('用户退出')
+  req.session.destroy()
+  console.log(`用户已退出网页，当前服务器的session内容为：${req.session}`)
+  res.redirect('/')
+})
+
+app.listen('9124', () => {
+  console.log('9124 running！')
+})
