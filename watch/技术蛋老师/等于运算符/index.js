3051a7da91a86e27858688c549eeb90dad5e2b18
zdnexus <zdnexus@163.com>
2022-12-13 23:10:16 +0800

    ~更新目录

---
+++ watch/技术蛋老师/等于运算符/index.js
@@ -0,0 +1,36 @@
+console.log(6 == 6)
+console.log(6 === 6)
+console.log(6 == '6')
+console.log(6 === '6')
+console.log()
+
+console.log(true == 1)
+console.log(true === 1)
+console.log(false == 0)
+console.log(false === 0)
+console.log()
+
+console.log('' == 0)
+console.log('   ' == 0)
+console.log(null == undefined)
+console.log(null == 0)
+console.log(undefined == 0)
+console.log(null == '')
+console.log(undefined == '')
+console.log()
+
+console.log('false' == false)
+console.log('false' === false)
+console.log(NaN == NaN)
+console.log(NaN === NaN)
+console.log(NaN == false)
+console.log(NaN === false)
+console.log()
+
+var a = {}
+var b = {}
+var c = a
+console.log(a == b)
+console.log(a == c)
+console.log(a === c)
+console.log()
